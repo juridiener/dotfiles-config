5d9ac7f521840b89abe1d47be0d5d6465c0b333a
Juri Diener <you@example.com>
2024-04-17 15:35:00 +0200

    fix issues with neorg

--- LazyVim/lua/plugins/neorg.lua
+++ LazyVim/lua/plugins/neorg.lua
@@ -1,40 +1,47 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {
-          config = {
-            icon_preset = "basic",
-          },
-        }, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/Documents/notes",
-              work = "~/Documents/notes/work",
-              private = "~/Documents/notes/private",
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  },
+  {
+
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {
+            config = {
+              icon_preset = "basic",
             },
-            default_workspace = "work",
+          }, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Documents/notes",
+                work = "~/Documents/notes/work",
+                private = "~/Documents/notes/private",
+              },
+              default_workspace = "work",
+            },
           },
-        },
-        ["core.export"] = {},
-        ["core.summary"] = {},
-        ["core.completion"] = {
-          config = {
-            engine = "nvim-cmp",
-            name = "[Neorg]",
+          ["core.export"] = {},
+          ["core.summary"] = {},
+          ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+              name = "[Neorg]",
+            },
           },
+          -- ["core.keybinds"] = {
+          --   config = {
+          --     neorg_leader = " ",
+          --   },
+          -- },
         },
-        -- ["core.keybinds"] = {
-        --   config = {
-        --     neorg_leader = " ",
-        --   },
-        -- },
-      },
-    })
-  end,
+      })
+    end,
+  },
 }
