d02a94b96a59b660f02926e7e573c636390b6392
Juri Diener <you@example.com>
2024-12-19 08:58:01 +0100

    lazyVim removed ncim-cmp to blink, for super-tab blink.lua

---
+++ LazyVim/lua/plugins/blink.lua
@@ -0,0 +1,15 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "enter",
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+    },
+    completion = {
+      list = {
+        selection = "auto_insert",
+      },
+    },
+  },
+}
