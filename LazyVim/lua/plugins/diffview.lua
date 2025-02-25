72175848472c68cae5922292d62e13f7f103cb23
Juri Diener <you@example.com>
2025-02-25 12:36:35 +0100

    new keys to diffview

--- LazyVim/lua/plugins/diffview.lua
+++ LazyVim/lua/plugins/diffview.lua
@@ -17,5 +17,15 @@ return {
     { "<leader>gdq", "<cmd>DiffviewClose<cr>", desc = "Close Git Diffview" },
     { "<leader>gdH", "<cmd>DiffviewFileHistory<cr>", desc = "History for the current branch" },
     { "<leader>gdh", "<cmd>DiffviewFileHistory %<cr>", desc = "History for the current file" },
+    {
+      "<leader>gdl",
+      "<cmd>DiffviewFileHistory --range=origin/main...HEAD --right-only --no-merges<cr>",
+      desc = "View local commits with remote",
+    },
+    {
+      "<leader>gdL",
+      "<cmd>DiffviewFileHistory --range=origin/main...HEAD --no-merges<cr>",
+      desc = "View branch differences with remote",
+    },
   },
 }
