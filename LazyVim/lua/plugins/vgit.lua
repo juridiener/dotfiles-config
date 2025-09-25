43bbfdb3254a371e89f759b7c37583880522c5e9
Juri Diener <you@example.com>
2025-09-25 11:41:52 +0200

    moved keymaps for vgit to plugin installation

--- LazyVim/lua/plugins/vgit.lua
+++ LazyVim/lua/plugins/vgit.lua
@@ -1,8 +1,34 @@
 return {
   "tanvirtin/vgit.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  event = { "BufReadPre", "BufNewFile" }, -- only load when a file is read or created
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("vgit").setup()
   end,
+  keys = {
+    {
+      "<leader>gvh",
+      function()
+        require("vgit").buffer_history_preview()
+      end,
+      desc = "Git history of current file",
+      silent = true,
+    },
+    {
+      "<leader>gvl",
+      function()
+        require("vgit").project_logs_preview()
+      end,
+      desc = "Project log",
+      silent = true,
+    },
+    {
+      "<leader>gvp",
+      function()
+        require("vgit").project_diff_preview()
+      end,
+      desc = "Project diff modified files",
+      silent = true,
+    },
+  },
 }
