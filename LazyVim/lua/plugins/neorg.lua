d7d8cd932a3dde2162984cd62a77fe0d56b3e5c3
Juri Diener <you@example.com>
2025-10-24 12:07:20 +0200

    fix neorg lua-utils error

--- LazyVim/lua/plugins/neorg.lua
+++ LazyVim/lua/plugins/neorg.lua
@@ -1,9 +1,9 @@
 return {
-  -- {
-  --   "vhyrro/luarocks.nvim",
-  --   priority = 1000, -- We'd like this plugin to load first out of the rest
-  --   config = true, -- This automatically runs `require("luarocks-nvim").setup()`
-  -- },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
   {
 
     "nvim-neorg/neorg",
